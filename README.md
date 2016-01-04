5653880a383ec309a8ee6fb9fd31d77786b91db4
Ekaterina Kuznetsova <ekate@meteor.com>
2016-01-04 15:17:32 -0800

    create a thin wrapper around dfischer:prerenderio

---
+++ README.md
@@ -0,0 +1,9 @@
+Galaxy SEO Support
+===
+
+Makes use of the built-in SEO support in Galaxy. If your app qualifies for SEO
+support, run:
+
+`meteor add galaxy:seo`
+
+to turn on the built-in Prerender.io integration.
---
+++ package.js
@@ -0,0 +1,16 @@
+Package.describe({
+  name: 'galaxy-seo-package',
+  version: '1.0.0',
+  // Brief, one-line summary of the package.
+  summary: 'Use built-in SEO support for enabled apps.',
+  // URL to the Git repository containing the source code for this package.
+  git: 'https://github.com/meteor/galaxy-seo-package',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.1');
+  api.imply('dfischer:prerenderio@1.0.4');
+});
